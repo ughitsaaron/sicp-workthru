cb392db64dee41915800c7a3f03db29b5dd622b0
Aaron Petcoff <hello@aaronpetcoff.me>
2018-10-01 16:38:24 -0400

    more ch1

---
+++ ch1/ch1.6.scm
@@ -0,0 +1,45 @@
+(define (square x) (* x x))
+
+(define (sqrt-iter guess x)
+  (define (average x y)
+    (/ (+ x y) 2))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+; (define (sqrt x)
+;   (sqrt-iter 1.0 x))
+
+; (sqrt 9)
+
+(define (new-if predicate then else-clase)
+  (cond (predicate then)
+    (else else-clause)))
+
+; (new-if (= 2 3) 0 5)
+; (new-if (= 1 1) 0 5)
+
+(define (sqrt-iter guess x)
+  (define (average x y)
+    (/ (+ x y) 2))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+  (new-if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+; `new-if` uses applicative order evaluation. it evaluates its operator and operands first and applies
+; the result to the arguments. this ends us up in an infinite loop when we call new-if in sqrt.
+
+; if is a special form that uses normal order evaluation. it evaluates the predicate before choosing
+; to evaluate the else clause. that prevents us from falling into an infinite loop.
+
--- ch1/notes.scm
+++ ch1/notes.scm
@@ -10,3 +10,50 @@
 ; the way we construct these systems in an understandable
 ; and manageable way is by "developing techniques for
 ; controlling complexity" <-- this is what cs is about!
+
+; substitution model
+; (define (+ x y)
+;   (if (= x 0)
+;     y ; if x is zero return y
+;     (+ (-1+ x) (1+ y)))) ; otherwise decrement the first operand and increment the second
+
+; (+ 3 4)
+; (+ 2 5)
+; (+ 1 6)
+; (+ 0 7)
+; 7
+; (LINEAR) ITERATIVE
+
+; (define (+ x y)
+;   (if (= x 0)
+;     y ; if x is zero return y
+;     (1+ (+ (-1+ x) y)))) ; otherwise increment the sum of the decrement of the first operand and the second
+
+; (+ 3 4)
+; (+ 2 (1+ 4))
+; (+ 1 (1+ (1+ 4)))
+; (+ 0 (1+ (1+ (1+ 4))))
+; (1+ (1+ 5))
+; (1+ 6)
+; 7
+; (LINEAR) RECURSIVE
+
+(define (fib n)
+  (if (< n 2)
+    n
+    (+ (fib (- n 1))
+       (fib (- n 2)))))
+
+(fib 6)
+
+(define (print-move from to)
+  (display (string "moving from " from))
+  (display (string "\n" "moving to " to "\n")))
+
+; towers of hanoi
+(define (move n from to spare)
+  (cond ((= n 0) "done")
+    (else
+      (move (-1+ n) from spare to)
+      (print-move from to)
+      (move (-1+ n) spare to from))))
